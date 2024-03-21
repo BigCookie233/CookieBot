1d35f3d58a58d514b53264af39a86ac1ceb4beb4
BigCookie233 <86756026+BigCookie233@users.noreply.github.com>
2024-03-21 21:04:01 +0800

    refactor: 修改日志命名方式

--- CookieLibraries/LoggerManager.py
+++ CookieLibraries/LoggerManager.py
@@ -35,13 +35,20 @@ def init(logs_path):
     global logger
     logger = logging.getLogger()
     logger.setLevel(logging.INFO)
-    log_name = "today.log"
+    log_name = "latest.log"
     log_path = os.path.join(logs_path, log_name)
     # 如果指定路径不存在，则尝试创建路径
     if not os.path.exists(logs_path):
         os.makedirs(logs_path)
 
+    def namer(filename):
+        dir_name, base_name = os.path.split(filename)
+        base_name = base_name.replace(log_name + '.', "")
+        rotation_filename = os.path.join(dir_name, base_name)
+        return rotation_filename
+
     file_handler = handlers.TimedRotatingFileHandler(log_path, when="MIDNIGHT", encoding="utf-8")
+    file_handler.namer = namer
     file_handler.suffix = "%Y-%m-%d.log"
     file_handler.setFormatter(logging.Formatter(fmt))
     logger.addHandler(file_handler)
