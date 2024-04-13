b59d7b13db7303c2bf35e588fa378597a692c603
BigCookie233 <86756026+BigCookie233@users.noreply.github.com>
2024-04-13 22:53:33 +0800

    fix: 修复异常处理装饰器参数类型检查

--- CookieLibraries/core/ExceptionHandlers.py
+++ CookieLibraries/core/ExceptionHandlers.py
@@ -9,7 +9,7 @@ from typing import Callable
 def exception_handler(handler: Callable) -> Callable:
     assert callable(handler), "the handler must be a callable object"
     params_len = len(inspect.signature(handler).parameters)
-    assert params_len > 1, f"the handler takes {params_len} positional arguments but only 1 at most will be given"
+    assert params_len <= 1, f"the handler takes {params_len} positional arguments but only 1 at most will be given"
 
     def wrapper(func: Callable) -> Callable:
         def catcher(*args, **kwargs):
