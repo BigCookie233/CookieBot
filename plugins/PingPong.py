961076a1e76bd3da10d3fb9869a55eb042388848
BigCookie233 <86756026+BigCookie233@users.noreply.github.com>
2024-06-01 22:17:25 +0800

    refactor: 更新示例插件

--- plugins/PingPong.py
+++ plugins/PingPong.py
@@ -3,6 +3,7 @@
 # Created by BigCookie233
 
 from CookieLibraries.core import *
+from CookieLibraries.extra import *
 from CookieLibraries.protocol import *
 
 PLUGIN_NAME = "PingPong"
@@ -11,7 +12,6 @@ PLUGIN_VERSION = "1.0-SNAPSHOT"
 logger = LoggerUtils.logger
 
 
-@EventManager.event_listener
-def on_message(event: MessageUtils.ReceiveGroupMessageEvent):
-    if event.startswith_atme() and event.segment_chain[1] == " ping!":
-        MessageUtils.MessageBuilder().at(event.sender.user_id).text(" pong!").send_to_group(event.group_id)
+@command_executor("ping")
+def on_message(sender: Sender, params: list):
+    sender.reply(MessageBuilder().at(sender.user_id).text(" pong!"))
