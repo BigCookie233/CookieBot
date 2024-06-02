587af92234fc4cd89bc7a67395009970b3706c96
BigCookie233 <86756026+BigCookie233@users.noreply.github.com>
2024-06-02 19:06:05 +0800

    refactor: 重构会话管理器

--- CookieLibraries/extra/SessionManager.py
+++ CookieLibraries/extra/SessionManager.py
@@ -13,14 +13,37 @@ _sessions = {}
 class Trigger(MessageListener):
     def __init__(self, matcher):
         super().__init__(None, matcher)
+        self.sender = None
+
+    def on_sender(self, sender):
+        self.sender = sender
+        return self
+
+    def __call__(self, event: ReceiveGroupMessageEvent):
+        if event.sender.user_id == self.sender.user_id:
+            super().__call__(event)
 
 
 class Session:
-    def __init__(self):
+    def __init__(self, creater):
         self.sender: Sender = None
         self.message = None
         self.trigger = None
         self.handler = None
+        self.creater = creater
+
+    def resume(self, sender, message):
+        self.sender = sender
+        self.message = message
+        try:
+            if self.trigger is not None:
+                self.trigger.unregister()
+            self.trigger = (next(self.handler)
+                               .set_callback(lambda p1, p2: self.resume(p1, p2))
+                               .on_sender(sender)
+                               .register())
+        except StopIteration:
+            del _sessions[id(self.creater)][sender.user_id]
 
 
 class SessionHandler(MessageListener):
@@ -30,27 +53,13 @@ class SessionHandler(MessageListener):
 
     def __call__(self, event: ReceiveGroupMessageEvent):
         if event.sender.user_id not in _sessions.get(id(self), {}):
-            session = Session()
-            session.message = self.matcher(event)
-            if session.message is not None:
-                session.sender = event.sender
+            session = Session(self)
+            message = self.matcher(event)
+            if message is not None:
+                sender = event.sender
                 session.handler = self.callback(session)
-                try:
-                    def func(sender, message):
-                        _session: Session = _sessions[id(self)][sender.user_id]
-                        _session.sender = sender
-                        _session.message = message
-                        try:
-                            _session.trigger.unregister()
-                            _session.trigger = next(_session.handler).set_callback(func).register()
-                        except StopIteration:
-                            del _sessions[id(self)][sender.user_id]
-                    session.trigger = next(session.handler).set_callback(func).register()
-                    _sessions.setdefault(id(self), {})[event.sender.user_id] = session
-                except StopIteration:
-                    del _sessions[id(self)][event.sender.user_id]
-
-
+                _sessions.setdefault(id(self), {})[event.sender.user_id] = session
+                session.resume(sender, message)
 
 
 def session_handler(matcher):
